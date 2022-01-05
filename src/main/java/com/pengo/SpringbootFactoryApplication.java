107695486091433181070e5d9a9cfbb4554e6368
pengo <961888137@qq.com>
2022-01-05 09:34:30 +0800

    first commit

---
+++ src/main/java/com/pengo/SpringbootFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.pengo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pengo/SpringbootFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pengo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
